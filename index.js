04d25e580e26338676078c377d6c8d2777d947de
minhl337 <imnotminh@gmail.com>
2019-03-04 23:00:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(people,things){
+  var pthings=[];
+  var i=0;
+  for(i=0;i<people.length;i++){
+    pthings.push(people[i]+' plays '+things[i])
+  }
+  return pthings;
+}
+
+function johnLennonFacts(facts){
+  var jj=[];
+  var i=0;
+  while(i<)
+}
